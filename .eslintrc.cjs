3e6e3f289cf4302d486f4336386aac55a9a3c279
ts-oh <tsohlove@gmail.com>
2022-07-28 10:52:07 +0700

    edit eslint to extend prettier and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,18 @@
 module.exports = {
 	env: {
 		browser: true,
-		es2021: true
+		es2021: true,
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:prettier/recommended',
-		'plugin:prettier/recommended'
-	],
+	extends: ['plugin:prettier/recommended'],
 	parserOptions: {
 		ecmaFeatures: {
-			jsx: true
+			jsx: true,
 		},
 		ecmaVersion: 'latest',
-		sourceType: 'module'
+		sourceType: 'module',
 	},
-	plugins: ['react'],
-	rules: {}
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': ['error'],
+	},
 };
